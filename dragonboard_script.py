02a21047aa5af642df3b52f87e1126786f0071fe
Hannan Saddiq <saddiqhb@gmail.com>
2019-03-24 08:03:56 +0000

    fixed serial communication

--- dragonboard_script.py
+++ dragonboard_script.py
@@ -3,24 +3,31 @@ import serial
 
 # Code for the Dragonboard to interface between the arduino and the main server interacting with the alexa
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = '192.168.43.231'  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
 
-#open serial connection
+# open serial connection
 
 ser = serial.Serial()
 
 ser.baudrate = 9600
-ser.port = "/dev/tty96B0"
+ser.port = "/dev/ttyACM0"
 ser.open()
+print("opened serial port with arduino")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("waiting to connect to server")
+    s.connect((HOST, PORT))
+    # wait to receive a payload from the server when required
     while True:
-        s.connect((HOST, PORT))
-        #wait to receive a payload from the server when required
         data = s.recv(1024)
+        print("Received '" + data.decode("utf-8") + "'")
 
-        #send the payload from the server to the arduino via serial
-        ser.write(str(str(data.decode("utf-8")) + "/n").encode("UTF-8"))
+        # send the payload from the server to the arduino via serial
+        ser.write(((data.decode("utf-8")) + "/n").encode("UTF-8"))
+        print("sent serial payload")
 
-print("Received '" + data.decode("utf-8") + "'")
+        arduino_string = ser.readline()
+        print(arduino_string)
+
+# print("Received '" + data.decode("utf-8") + "'")
--- socket_server.py
+++ socket_server.py
@@ -1,19 +1,23 @@
 import socket
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
 
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
+        s.bind(("", PORT))
+        s.listen(0)
+        print("waiting to connect to client")
         conn, addr = s.accept()
+        print("got connection with client")
         with conn:
             print('Connected by', addr)
             while True:
-                data = conn.recv(1024)
-                if data:
-                    print(repr(data))
-                else:
-                    break
-                conn.sendall(data)
+                # data = conn.recv(1024)
+                # if data:
+                #     print(repr(data))
+                # else:
+                #     break
+
+                conn.sendall(b"101010000010011011")
